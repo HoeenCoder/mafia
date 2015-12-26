4bda66f69ab4a8d8d263557f336f4969d06736f5
Bär Halberkamp <fledaway@gmail.com>
2015-12-26 18:42:01 +0100

    UUUUUGH

--- generator.js
+++ generator.js
@@ -2,11 +2,11 @@ var classesDiv = document.getElementById('classes');
 var settingsDiv = document.getElementById('settings');
 
 
-function createClassDiv(class) {
+function createClassDiv(classData) {
     var classDiv = document.createElement('div');
     classDiv.setAttribute('class','mafiaclass');
-    classDiv.setAttribute('id',class.id);
-    classDiv.innerHTML = '<table><tr><td style="text-align:center;"><img src="images/' + class.id + '.png"></td><td style="width:80%;">' + class.name + '</td><td><input type="number" name="' + class.id + 'num" value="0" min="0" max="25"></td></tr></table>';
+    classDiv.setAttribute('id',classData.id);
+    classDiv.innerHTML = '<table><tr><td style="text-align:center;"><img src="images/' + classData.id + '.png"></td><td style="width:80%;">' + classData.name + '</td><td><input type="number" name="' + classData.id + 'num" value="0" min="0" max="25"></td></tr></table>';
     classesDiv.appendChild(classDiv);
 }
 
