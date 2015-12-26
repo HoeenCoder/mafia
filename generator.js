a31454c68440e3c18b9bc350b0a7e80a0756e608
Bär Halberkamp <fledaway@gmail.com>
2015-12-26 20:51:18 +0100

    ok

--- generator.js
+++ generator.js
@@ -1,11 +1,11 @@
 var classesDiv, settingsDiv;
 
+var result = {classes: {}, settings: {}};
 
 function createClassDiv(classData) {
     var classDiv = document.createElement('div');
     classDiv.setAttribute('class','mafiaclass');
-    classDiv.setAttribute('id',classData.id);
-    classDiv.innerHTML = '<div style="text-align:center;"><img src="images/' + classData.id + '.png"></div><div class="classname">' + classData.name + '</div><input type="number" name="' + classData.id + 'num" value="0" min="0" max="25"></div>';
+    classDiv.innerHTML = '<div style="text-align:center;"><img src="images/' + classData.id + '.png"></div><div class="classname">' + classData.name + '</div><input type="number" name="' + classData.id + '" value="0" min="0" max="25"></div>';
     classesDiv.appendChild(classDiv);
 }
 
@@ -15,4 +15,29 @@ function generateClasses() {
     for (var i = 0; i < Data.classes.length; i++) {
         createClassDiv(Data.classes[i]);
     }
+}
+
+function updateResult() {
+    var resultElem = document.getElementById('results');
+    resultElem.innerHTML = JSON.stringify(result);
+}
+
+function changeClass(element) {
+    if (element.value) {
+        result.settings[element.name] = element.value;
+    } else if (result.settings[element.name]) {
+        delete result.settings[element.name];
+    }
+
+    updateResult();
+}
+
+function changeSetting(element) {
+    if (element.checked) {
+        result.settings[element.name] = true;
+    } else if (result.settings[element.name]) {
+        delete result.settings[element.name];
+    }
+
+    updateResult();
 }
