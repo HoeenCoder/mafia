8b861b1dab0b62c6f63d625da4829b8c62ef0a6b
Bär Halberkamp <fledaway@gmail.com>
2015-12-26 19:41:01 +0100

    Add some styles

--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ function createClassDiv(classData) {
     var classDiv = document.createElement('div');
     classDiv.setAttribute('class','mafiaclass');
     classDiv.setAttribute('id',classData.id);
-    classDiv.innerHTML = '<table><tr><td style="text-align:center;"><img src="images/' + classData.id + '.png"></td><td style="width:80%;">' + classData.name + '</td><td><input type="number" name="' + classData.id + 'num" value="0" min="0" max="25"></td></tr></table>';
+    classDiv.innerHTML = '<table><tr><td style="text-align:center;"><img src="images/' + classData.id + '.png"></td><td class="classname">' + classData.name + '</td><td><input type="number" name="' + classData.id + 'num" value="0" min="0" max="25"></td></tr></table>';
     classesDiv.appendChild(classDiv);
 }
 
