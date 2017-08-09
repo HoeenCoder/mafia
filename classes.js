92fd7e20665e9c6e21fd4e838f64fcc4170e400f
HoeenCoder <HoeenCoder@users.noreply.github.com>
2017-08-09 01:37:38 +0000

    Update generator

--- classes.js
+++ classes.js
@@ -2,43 +2,410 @@ var Data = {
   "classes": [
     {
       "id": "villager",
-      "name": "Villager"
+      "name": "Villager",
+      "show": ["town"]
     },
     {
       "id": "mafia",
-      "name": "Mafia"
+      "name": "Mafia",
+      "show": ["mafia"]
+    },
+    {
+      "id": "werewolf",
+      "name": "Werewolf",
+      "show": ["werewolf"]
+    },
+    {
+      "id": "alien",
+      "name": "Alien",
+      "show": ["alien"]
+    },
+    {
+      "id": "cultmember",
+      "name": "Cult Member",
+      "show": ["cult"]
+    },
+    {
+      "id": "survivor",
+      "name": "Survivor",
+      "show": ["solo"]
+    },
+    {
+      "id": "serialkiller",
+      "name": "Serial Killer",
+      "show": ["solo"]
+    },
+    {
+      "id": "watcher",
+      "name": "Watcher"
+    },
+    {
+      "id": "tracker",
+      "name": "Tracker"
     },
     {
       "id": "cop",
       "name": "Cop"
     },
     {
-      "id": "hooker",
-      "name": "Hooker"
+      "id": "seer",
+      "name": "Seer"
+    },
+    {
+      "id": "fbiagent",
+      "name": "FBI Agent"
+    },
+    {
+      "id": "retiredwerewolfhunter",
+      "name": "Retired Werewolf Hunter",
+      "noShow": ["werewolf"]
     },
     {
       "id": "doctor",
       "name": "Doctor"
     },
     {
-      "id": "werewolf",
-      "name": "Werewolf"
+      "id": "roleblocker",
+      "name": "Roleblocker"
     },
     {
-      "id": "mayor",
-      "name": "Mayor"
+      "id": "jailkeeper",
+      "name": "Jailkeeper"
+    },
+    {
+      "id": "bodyguard",
+      "name": "Bodyguard"
+    },
+    {
+      "id": "vigilante",
+      "name": "Vigilante"
+    },
+    {
+      "id": "childkiller",
+      "name": "Childkiller"
+    },
+    {
+      "id": "jackofalltrades",
+      "name": "Jack-of-all-Trades"
+    },
+    {
+      "id": "blackgoo",
+      "name": "Black Goo"
+    },
+    {
+      "id": "privateinvestigator",
+      "name": "Private Investigator"
+    },
+    {
+      "id": "nymphomaniac",
+      "name": "Nymphomaniac"
+    },
+    {
+      "id": "governor",
+      "name": "Governor"
+    },
+    {
+      "id": "innocentchild",
+      "name": "Innocent Child"
+    },
+    {
+      "id": "hider",
+      "name": "Hider"
+    },
+    {
+      "id": "enabler",
+      "name": "Enabler"
+    },
+    {
+      "id": "treestump",
+      "name": "Treestump"
+    },
+    {
+      "id": "conspiracytheorist",
+      "name": "Conspiracy Theorist"
+    },
+    {
+      "id": "bloodhound",
+      "name": "Bloodhound"
+    },
+    {
+      "id": "vanillacop",
+      "name": "Vanilla Cop"
+    },
+    {
+      "id": "tourist",
+      "name": "Tourist"
+    },
+    {
+      "id": "nurse",
+      "name": "Nurse"
+    },
+    {
+      "id": "commuter",
+      "name": "Commuter"
+    },
+    {
+      "id": "copofalltrades",
+      "name": "Cop-of-all-Trades"
+    },
+    {
+      "id": "psychiatrist",
+      "name": "Psychiatrist"
+    },
+    {
+      "id": "reloader",
+      "name": "Reloader"
+    },
+    {
+      "id": "fruitvendor",
+      "name": "Fruit Vendor"
+    },
+    {
+      "id": "parrot",
+      "name": "Parrot"
+    },
+    {
+      "id": "cupid",
+      "name": "Cupid"
+    },
+    {
+      "id": "prober",
+      "name": "Prober"
+    },
+    {
+      "id": "vanillaiser",
+      "name": "Vanillaiser"
+    },
+    {
+      "id": "silencer",
+      "name": "Silencer"
+    },
+    {
+      "id": "psychotrooper",
+      "name": "Psychotrooper"
+    },
+    {
+      "id": "redirector",
+      "name": "Redirector"
+    },
+    {
+      "id": "bloodsucker",
+      "name": "Bloodsucker"
+    },
+    {
+      "id": "recruiter",
+      "name": "Recruiter"
+    },
+    {
+      "id": "goomaker",
+      "name": "Goomaker"
+    },
+    {
+      "id": "judas",
+      "name": "Judas",
+      "show": ["town"]
+    },
+    {
+      "id": "saulus",
+      "name": "Saulus",
+      "show": ["mafia"]
+    },
+    {
+      "id": "lyncher",
+      "name": "Lyncher",
+      "show": ["solo"]
+    },
+    {
+      "id": "wildcard",
+      "name": "Wild Card",
+      "show": ["wildcard"]
+    },
+    {
+      "name": "Underdog",
+      "id": "underdog",
+      "show": ["solo"]
+    },
+    {
+      "name": "Alien Sympathiser",
+      "id": "aliensympathiser",
+      "show": ["alien"]
+    },
+    {
+      "name": "One-Shot Townie",
+      "id": "oneshowtownie",
+      "show": ["town"]
+    },
+  ],
+  "modifiers": [
+    {
+      id: "lover",
+      name: "Lover",
+      noShotLimit: true
+    },
+    {
+      id: "day",
+      name: "Day",
+      noShotLimit: true
+    },
+    {
+      id: "compulsive",
+      name: "Compulsive",
+    },
+    {
+      id: "bulletproof",
+      name: "Bulletproof",
+    },
+    {
+      id: "supersaint",
+      name: "Supersaint",
+      noShotLimit: true
+    },
+    {
+      id: "paranoidgunowner",
+      name: "Paranoid Gun Owner",
+    },
+    {
+      id: "mason",
+      name: "Mason",
+      noShotLimit: true,
+      noShow: ["mafia"]
+    },
+    {
+      id: "monk",
+      name: "Monk",
+      noShotLimit: true,
+      noShow: ["werewolf"]
+    },
+    {
+      id: "vengeful",
+      name: "Vengeful",
+      noShotLimit: true,
+    },
+    {
+      id: "retiredmarine",
+      name: "Retired Marine",
+    },
+    {
+      id: "miller",
+      name: "Miller",
+    },
+    {
+      id: "hirsute",
+      name: "Hirsute",
+    },
+    {
+      id: "evangelistic",
+      name: "Evangelistic Townie",
+    },
+    {
+      id: "tentacled",
+      name: "Tentacled",
+    },
+    {
+      id: "watchlisted",
+      name: "Watchlisted",
+    },
+    {
+      id: "wrongplaceatthewrongtime",
+      name: "Wrong Place at the Wrong Time",
+    },
+    {
+      id: "blackgoo",
+      name: "Black Goo",
+    },
+    {
+      id: "ascetic",
+      name: "Ascetic",
+    },
+    {
+      "id": "unlynchable",
+      "name": "Unlynchable"
     },
     {
       "id": "godfather",
       "name": "Godfather"
     },
     {
-      "id": "fool",
-      "name": "Fool"
+      id: "gravedigger",
+      name: "Gravedigger",
     },
     {
-      "id": "goon",
-      "name": "Goon"
+      id: "weak",
+      name: "Weak",
+    },
+    {
+      id: "hero",
+      name: "Hero",
+    },
+    {
+      id: "lynchbait",
+      name: "Lynchbait",
+    },
+    {
+      id: "strongman",
+      name: "Strongman",
+    },
+    {
+      id: "reflexive",
+      name: "Reflexive",
+    },
+    {
+      id: "alpha",
+      name: "Alpha",
+    },
+    {
+      id: "ninja",
+      name: "Ninja",
+    },
+    {
+      id: "mass",
+      name: "Mass"
+    },
+    {
+      id: "immunetomafiakills",
+      name: "Immune to Mafia Kills"
+    },
+    {
+      id: "immunetowerewolfkills",
+      name: "Immune to Werewolf Kills"
+    },
+    {
+      id: "immunetoalienkills",
+      name: "Immune to Alien Kills"
+    },
+  ],
+  "alignments": [
+    {
+      name: "Town",
+      id: "town"
+    },
+    {
+      name: "Mafia",
+      id: "mafia"
+    },
+    {
+      name: "Werewolf",
+      id: "werewolf"
+    },
+    {
+      name: "Alien",
+      id: "alien"
+    },
+    {
+      name: "Survivor",
+      id: "survivor"
+    },
+    {
+      name: "Cult",
+      id: "cult"
+    },
+    {
+      name: "Solo",
+      id: "solo"
+    },
+    {
+      name: "Wild Card",
+      id: "wildcard"
     }
   ]
 };
--- generator.js
+++ generator.js
@@ -1,40 +1,164 @@
-var classesDiv, settingsDiv;
+var addDiv, settingsDiv, side = "town";
 
-var result = {classes: {}, settings: {}};
+var result = {rolelist: [], settings: {}};
 
-function createClassDiv(classData) {
-    var classDiv = document.createElement('div');
-    classDiv.setAttribute('class','mafiaclass');
-    classDiv.innerHTML = '<div style="text-align:center;"><img src="images/' + classData.id + '.png"></div><div class="classname">' + classData.name + '</div><input type="number" onchange="changeClass(this)" name="' + classData.id + '" value="0" min="0" max="25"></div>';
-    classesDiv.appendChild(classDiv);
+function generate() {
+    addDiv = document.getElementById('addRole');
+    
+    var out = '<details id="addRoleDetail"><summary cursor="pointer">Add a role</summary><br/>';
+    
+    out += '<br/>Choose an Alignment <b>(will reload chooseable roles and modifiers)</b>: <select id="alignSelect" onChange="updateAvaliableOptions(this)">';
+    for (var k = 0; k < Data.alignments.length; k++) {
+        out += '<option value="' + Data.alignments[k].id + '">' + Data.alignments[k].name + '</option>';
+    }
+    
+    out += '</select><br/><br/>Choose a role: <select id="roleSelect">';
+    out += roleSelectHTML();
+    
+    out += '</select><br/><br/>Choose Modifiers (if any, max of 5)<br/><b>Set Shot value to 0 for unlimited use</b><form id="modifiersForm">';
+    out += modifiersHTML();
+    
+    out += '</form><br/>How many to add:<form><input id="roleNum" type="number" value="1" min="1" max="25"></form><br/>';
+    out += 'Name of role (leave blank for default, I recommend you name the roles you use):<form><input id="roleNameInput" type="text" size="40"/></form><br/>';
+    
+    out += '<button onClick="addRole()">Add Role(s)</button></details>';
+    
+    addDiv.innerHTML = out;
 }
 
-function generateClasses() {
-    classesDiv = document.getElementById('classes');
+function updateAvaliableOptions(element) {
+    if (!element.value) return;
+    side = element.value;
+    var roleSelect = document.getElementById('roleSelect');
+    roleSelect.innerHTML = roleSelectHTML();
+    var modifiersForm = document.getElementById('modifiersForm');
+    modifiersForm.innerHTML = modifiersHTML();
+}
 
+function roleSelectHTML() {
+    var out = '';
     for (var i = 0; i < Data.classes.length; i++) {
-        createClassDiv(Data.classes[i]);
+        if (side === 'wildcard' && Data.classes[i].id !== 'wildcard') continue; // Special rules for Wild Card
+        if (Data.classes[i].show) {
+            if (Data.classes[i].show.indexOf(side) === -1) continue;
+        }
+        if (Data.classes[i].noShow) {
+            if (Data.classes[i].noShow.indexOf(side) > -1) continue;
+        }
+        out += '<option value="' + Data.classes[i].id + '">' + Data.classes[i].name + '</option>';
     }
+    return out;
+}
+
+function modifiersHTML() {
+    var out = '<details><summary cursor="pointer">Modifiers</summary>';
+    for (var j = 0; j < Data.modifiers.length; j++) {
+        if (side === 'wildcard') return '<br/><b>No modifiers avaliable for Wild Card</b>';
+        if (Data.modifiers[j].show) {
+            if (Data.modifiers[j].show.indexOf(side) === -1) continue;
+        }
+        if (Data.modifiers[j].noShow) {
+            if (Data.modifiers[j].noShow.indexOf(side) > -1) continue;
+        }
+        out += '<input type="checkbox" name="' + Data.modifiers[j].id + '"/>';
+        if (!Data.modifiers[j].noShotLimit) {
+            out += '<input name="' + Data.modifiers[j].id + '" type="number" value="0" min="0" max="25"/>-Shot ' + Data.modifiers[j].name + '<br/>';
+        } else {
+            out += Data.modifiers[j].name + '<br/>'
+        }
+    }
+    out += '</details>';
+    return out;
+}
+
+function defaultName(role) {
+    var out = '';
+    for (let i in role.modifiers) {
+        if (role.modifiers[i]) {
+            out += role.modifiers[i] + '-Shot ' + i + ' ';
+        } else {
+            out += i + ' ';
+        }
+    }
+    out += role.role;
+    return out;
+}
+
+function addRole() {
+    var role = {
+        num: parseInt(document.getElementById('roleNum').value),
+        role: document.getElementById('roleSelect').value,
+        side: document.getElementById('alignSelect').value,
+        name: document.getElementById('roleNameInput').value.trim(),
+        modifiers: {}
+    };
+    if (isNaN(role.num)) role.num = 1;
+    var form = document.getElementById('modifiersForm'), numMods = 0;
+    for (let m = 0; m < form.length; m++) {
+        let mod = form[m];
+        if (mod.type === 'checkbox') {
+            if (mod.checked) {
+                role.modifiers[mod.name] = 0;
+                numMods++;
+            }
+        } else {
+            if (role.modifiers.hasOwnProperty(mod.name)) role.modifiers[mod.name] = parseInt(mod.value);
+        }
+        if (numMods > 5) {
+            alert('You cannot have more than 5 role modifiers on a single role!');
+            return;
+        }
+    }
+    if (!role.name) role.name = defaultName(role);
+    role.id = role.name.toLowerCase().replace(/[^a-z0-9]+/g, '');
+    for (var i = 0; i < result.rolelist.length; i++) {
+        if (result.rolelist[i].id === role.id) {
+            alert('You can\'t have two roles with the same name! (' + role.name + ') If you want to add more of this role, use the buttons in the rolelist.');
+            return;
+        }
+    }
+    console.log(role);
+    result.rolelist.push(role);
+    updateResult();
+    updateRoles();
+    document.getElementById('addRoleDetail').removeAttribute("open");
+    generate();
+}
+
+function incrementRole(el, amount) {
+    var id = el.id
+    amount = parseInt(amount);
+    if (isNaN(amount) || amount < -10 || amount > 10 || amount === 0) return;
+    for (var i = 0; i < result.rolelist.length; i++) {
+        if (result.rolelist[i].id === id) {
+            result.rolelist[i].num += amount;
+            if (result.rolelist[i].num < 1) result.rolelist[i].num = 1;
+            break;
+        }
+    }
+    updateResult();
+    updateRoles();
+}
+
+function removeRole(el) {
+    var id = el.id;
+    for (var i = 0; i < result.rolelist.length; i++) {
+        if (result.rolelist[i].id === id) {
+            result.rolelist.splice(i, 1);
+            break;
+        }
+    }
+    updateResult();
+    updateRoles();
 }
 
 function updateResult() {
     var resultElem = document.getElementById('results');
-    if (Object.keys(result.classes).length) {
-        resultElem.innerHTML = "/mafia new " + JSON.stringify(result);
+    if (result.rolelist.length) {
+        resultElem.innerHTML = "/mafia setroles " + JSON.stringify(result);
     } else {
         resultElem.innerHTML = "Please select classes to use.";
     }
-}
-
-function changeClass(element) {
-    var val = parseInt(element.value);
-    if (val) {
-        result.classes[element.name] = val;
-    } else if (result.classes[element.name]) {
-        delete result.classes[element.name];
-    }
-
-    updateResult();
 }
 
 function changeSetting(element) {
@@ -45,4 +169,15 @@ function changeSetting(element) {
     }
 
     updateResult();
+}
+
+function updateRoles() {
+    let roles = document.getElementById('roles');
+    let out = '';
+    for (var i = 0; i < result.rolelist.length; i++) {
+        out += '<div><b>' + result.rolelist[i].num + ' ' + result.rolelist[i].name + '</b> <button id="' + result.rolelist[i].id + '" onClick="incrementRole(this, 1)">Add one</button> ';
+        if (result.rolelist[i].num > 1) out += '<button id="' + result.rolelist[i].id + '" onClick="incrementRole(this, -1)">Remove one</button> ';
+        out += '<button id="' + result.rolelist[i].id + '" onClick="removeRole(this)">Remove this role</button></div>';
+    }
+    roles.innerHTML = out;
 }
